ebce505f75cf291af0db61d5a8eeecb9fa1c74d6
olderthanold <olderthanold@gmail.com>
2026-04-02 19:25:12 +0200

    update

--- git_mbin.sh
+++ git_mbin.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -d "$HOME"/mbin/.git ]; then  # If repo exists, pull
-  git -C "$HOME"/mbin pull origin main
+  if ! git -C "$HOME"/mbin pull origin main; then
+    echo "pull failed"
+    rm -rf "$HOME"/mbin/.git
+    git clone -b main https://github.com/olderthanold/mbin.git "$HOME"/mbin
+  fi
 else  # If missing, clone
   git clone -b main https://github.com/olderthanold/mbin.git "$HOME"/mbin
 fi
