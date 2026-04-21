0cd0631dc9dd15b20b41f322e73126738f8ba0f1
olderthanold <olderthanold@gmail.com>
2026-04-21 13:31:45 +0200

    Update mbin 2026-04-21 13:31:45.65

--- ai/run_ai.sh
+++ ai/run_ai.sh
@@ -22,5 +22,7 @@ llama-cli -hf unsloth/gemma-3-1b-it-GGUF:Q3_K_S --host 10.0.0.11 --port 8080
 llama-cli -hf Andycurrent/Gemma-3-1B-it-GLM-4.7-Flash-Heretic-Uncensored-Thinking_GGUF:Q3_K_M -hff gemma-3-1b-it-Q4_K_M.gguf  --host 10.0.0.11 --port 8080
 
 llama-cli -hf unsloth/Qwen3.5-0.8B-GGUF:Q3_K_S --host 10.0.0.11 --port 8080
-llama-cli -hf bartowski/Qwen_Qwen3.5-0.8B-GGUF:Q3_K_S --host 10.0.0.11 --port 8080
 llama-cli -hf Jackrong/Qwen3.5-0.8B-Claude-4.6-Opus-Reasoning-Distilled-GGUF:Q3_K_S --host 10.0.0.11 --port 8080
+
+
+/home/ubun2/ai/llama.cpp/build/bin/llama-cli -hf bartowski/Qwen_Qwen3.5-0.8B-GGUF:Q3_K_S --reasoning off --no-mmproj
--- mstats.sh
+++ mstats.sh
@@ -1,9 +1,14 @@
+echo -e "\033[1;34m Starting mstats... \033[0m"
 echo -e "\033[0;32m =---------- ps -la ----------= \033[0m"
 ps -la
-echo -e "\033[0;32m =---------- echo -e ----------= \033[0m"
+echo -e "\033[0;32m =---------- swap show ----------= \033[0m"
 swapon --show
 echo -e "\033[0;32m =---------- free -h ----------= \033[0m"
 free -h
 echo -e "\033[0;32m =---------- df -h /dev/sda1 ----------= \033[0m"
 df -h /dev/sda1
+echo -e "\033[0;32m =---------- hostname -I ----------= \033[0m"
+hostname -I
+echo -e "\033[0;32m =---------- public IP (ifconfig.me) ----------= \033[0m"
+curl ifconfig.me
 echo -e "\033[1;33m =---------- stats done ----------= \033[0m"
