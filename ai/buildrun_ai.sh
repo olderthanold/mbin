5bb4ab1a862a47f18869e644496d3f1ac36a5602
olderthanold <olderthanold@gmail.com>
2026-04-22 01:50:43 +0200

    Update mbin 2026-04-22  1:50:43.05

--- ai/buildrun_ai.sh
+++ ai/buildrun_ai.sh
@@ -1,3 +1,3 @@
 cd /home/ubun2
-nohup sudo -E bash -c "export PATH=$PATH; /opt/mbin/ai/buildrun_ai.sh" > /home/ubun2/aibuild.log 2>&1 && touch aibuild_script_completed.txt &
+nohup sudo -E bash -c "export PATH=$PATH; /opt/mbin/ai/aibuild.sh" > /home/ubun2/aibuild.log 2>&1 && touch aibuild_script_completed.txt &
 echo $!
