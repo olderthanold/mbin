02fd3de29ff0fca9d597979e5e2595766d91de04
olderthanold <olderthanold@gmail.com>
2026-04-21 13:43:20 +0200

    Update mbin 2026-04-21 13:43:20.22

--- mstats.sh
+++ mstats.sh
@@ -7,8 +7,7 @@ echo -e "\033[0;32m =---------- free -h ----------= \033[0m"
 free -h
 echo -e "\033[0;32m =---------- df -h /dev/sda1 ----------= \033[0m"
 df -h /dev/sda1
-echo -e "\033[0;32m =---------- hostname -I ----------= \033[0m"
-hostname -I
+echo -e "\033[0;32m =----- hostname -I: \033[0m" & hostname -I
 echo -e "\033[0;32m =---------- public IP (ifconfig.me) ----------= \033[0m"
 curl ifconfig.me
 echo -e "\033[1;33m =---------- stats done ----------= \033[0m"
